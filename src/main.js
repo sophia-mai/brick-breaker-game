4a37fa72dc663b68ee91b3c4a89cebe3fef40191
unknown <sophiathanhmai@gmail.com>
2026-02-28 14:23:28 -0500

    brick-02: Animate the ball with requestAnimationFrame

--- src/main.js
+++ src/main.js
@@ -1,1 +1,31 @@
 import "./style.css";
+
+const canvas = document.getElementById("myCanvas");
+const ctx = canvas.getContext("2d");
+
+ctx.beginPath();
+ctx.rect(20, 40, 50, 50);
+ctx.fillStyle = "#FF0000";
+ctx.fill();
+ctx.closePath();
+
+let x = canvas.width / 2;
+let y = canvas.height - 30;
+const dx = 2;
+const dy = -2;
+
+function draw() {
+  ctx.clearRect(0, 0, canvas.width, canvas.height);
+  ctx.beginPath();
+  ctx.rect(x, y, 10, 10);
+  ctx.fillStyle = "#0095DD";
+  ctx.fill();
+  ctx.closePath();
+
+  x += dx;
+  y += dy;
+
+  window.requestAnimationFrame(draw);
+}
+
+draw();
